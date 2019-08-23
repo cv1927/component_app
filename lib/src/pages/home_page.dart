bcf641254e3b5e498d014c3cd74f5c025e1051a6
Camilov1927 <camilov1927@gmail.com>
2019-08-22 23:30:03 -0500

    se agrego el future builder a la aplicación

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -15,23 +15,59 @@ class HomePage extends StatelessWidget {
 
   Widget _lista() {
     
-    print( menuProvider.opciones );
+    //print( menuProvider.opciones );
+    /*menuProvider.cargarData().then((opciones) {
 
-    return ListView(
-      children: _listaItems(),
+      print('lista');
+      print(opciones);
+
+    });*/
+
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      initialData: [],
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+
+        return ListView(
+          children: _listaItems( snapshot.data ),
+        );
+
+      },
     );
+
+    /*return ListView(
+      children: _listaItems(),
+    );*/
 
   }
 
-  List<Widget> _listaItems() {
+  List<Widget> _listaItems( List<dynamic> data ) {
 
-    return [
+    /*return [
       ListTile( title: Text('Hola Mundo'),),
       Divider(),
       ListTile( title: Text('Hola Mundo'),),
       Divider(),
       ListTile( title: Text('Hola Mundo'),),
-    ];
+    ];*/
 
+    final List<Widget> opciones = [];
+
+    data.forEach((opt) {
+      final widgetTemp = ListTile(
+        title: Text( opt['texto'] ),
+        leading: Icon(Icons.account_circle, color: Colors.blue,),
+        trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue,),
+        onTap: () {
+
+        },
+      );
+
+      opciones..add(widgetTemp)
+              ..add( Divider() );
+    });
+
+
+    return opciones;
   }
 }
